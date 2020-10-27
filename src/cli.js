b47d378bf093f45e0bbead10fa9452ff9212c84e
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2020-10-27 05:06:28 +0100

    init completium cli

--- src/cli.js
+++ src/cli.js
@@ -1,68 +1,42 @@
 import arg from 'arg';
 import inquirer from 'inquirer';
-import { createProject } from './main';
+import { process } from './main';
 
 function parseArgumentsIntoOptions(rawArgs) {
   const args = arg(
     {
-      '--git': Boolean,
-      '--yes': Boolean,
-      '--install': Boolean,
-      '-g': '--git',
       '-y': '--yes',
-      '-i': '--install',
+      '-a:' : '--account',
+      '-f' : '--file',
+      '--init': Boolean,
+      '--deploy': Boolean,
     },
     {
       argv: rawArgs.slice(2),
     }
   );
   return {
-    skipPrompts: args['--yes'] || false,
-    git: args['--git'] || false,
-    template: args._[0],
-    runInstall: args['--install'] || false,
+    init:    args['--init'] || false,
+    deploy:  args['--deploy'] || false,
+    account: args['--account'],
+    file:    args['--file'],
   };
 }
 
 async function promptForMissingOptions(options) {
-  const defaultTemplate = 'JavaScript';
-  if (options.skipPrompts) {
-    return {
-      ...options,
-      template: options.template || defaultTemplate,
-    };
-  }
 
-  const questions = [];
-  if (!options.template) {
-    questions.push({
-      type: 'list',
-      name: 'template',
-      message: 'Please choose which project template to use',
-      choices: ['JavaScript', 'TypeScript'],
-      default: defaultTemplate,
-    });
-  }
+  // const questions = [];
 
-  if (!options.git) {
-    questions.push({
-      type: 'confirm',
-      name: 'git',
-      message: 'Initialize a git repository?',
-      default: false,
-    });
-  }
-
-  const answers = await inquirer.prompt(questions);
-  return {
-    ...options,
-    template: options.template || answers.template,
-    git: options.git || answers.git,
-  };
+  // const answers = await inquirer.prompt(questions);
+  // return {
+  //   ...options,
+  //   template: options.template || answers.template,
+  //   git: options.git || answers.git,
+  // };
 }
 
 export async function cli(args) {
   let options = parseArgumentsIntoOptions(args);
-  options = await promptForMissingOptions(options);
-  await createProject(options);
+  // options = await promptForMissingOptions(options);
+  await process(options);
 }
--- src/main.js
+++ src/main.js
@@ -1,75 +1,98 @@
 import chalk from 'chalk';
 import fs from 'fs';
-import ncp from 'ncp';
-import path from 'path';
-import { promisify } from 'util';
-import execa from 'execa';
+import wget from 'node-wget';
 import Listr from 'listr';
-import { projectInstall } from 'pkg-install';
+import execa from 'execa';
 
-const access = promisify(fs.access);
-const copy = promisify(ncp);
+const completium_dir = '/tmp/.completium'
+const bin_dir        = completium_dir + '/bin'
+const bin_archetype  = bin_dir + '/archetype'
+const bin_tezos      = bin_dir + "/tezos-client"
 
-async function copyTemplateFiles(options) {
- return copy(options.templateDirectory, options.targetDirectory, {
-   clobber: false,
- });
+async function download(url, dest) {
+  const request = wget({ url: url, dest: dest, timeout: 2000 });
+  return request;
+};
+
+async function initCompletium(options) {
+
+  if (!fs.existsSync(bin_dir)) {
+    fs.mkdirSync(bin_dir);
+  }
+
+  const archetype_url   = "https://github.com/edukera/archetype-lang/releases/download/1.2.1/archetype-x64-linux";
+  const tezosclient_url = "https://github.com/serokell/tezos-packaging/releases/latest/download/tezos-client";
+  const a = await download(tezosclient_url, bin_tezos);
+  const b = await download(archetype_url,   bin_archetype);
+  fs.chmodSync(bin_archetype, '711');
+  fs.chmodSync(bin_tezos,     '711');
+  console.log("Completium initialized successfully!");
+  return;
 }
 
-async function initGit(options) {
- const result = await execa('git', ['init'], {
-   cwd: options.targetDirectory,
- });
- if (result.failed) {
-   return Promise.reject(new Error('Failed to initialize git'));
- }
- return;
+async function deploy(options) {
+  const tz_sci = "tz1Lc2qBKEWCBeDU8npG6zCeCqpmaegRi6Jg";
+  // const arl = options.file;
+  const arl = "/home/dev/archetype/archetype-lang/tests/passed/simple.arl";
+  const contract_name = "simple";
+  const contracts_dir = completium_dir + "/contracts";
+  const contract_script = contracts_dir + contract_name + ".tz";
+  const tezos_node = "testnet-tezos.giganode.io";
+  const tezos_port = "443";
+
+  {
+    const { stdout } = await execa(bin_archetype, [arl], {});
+    console.log(stdout);
+
+    fs.writeFile(contract_script, stdout, function (err) {
+      if (err) throw err;
+      console.log('Contract script saved!');
+    });
+  }
+
+  var tzstorage = "";
+  {
+    const { stdout } = await execa(bin_archetype, ['-t', 'michelson-storage', '-sci', tz_sci, arl], {});
+    tzstorage = stdout;
+    console.log(tzstorage);
+  }
+
+  {
+    const { stdout } = await execa(bin_tezos,
+      ['-S', '-A', tezos_node,
+        '-P', tezos_port,
+        'originate', 'contract', contract_name,
+        'transferring', '0',
+        'from', tz_sci,
+        'running', contract_script,
+        '--init', tzstorage,
+        '--burn-cap', '20',
+        '--force',
+        '-D'
+      ], {});
+    console.log(stdout);
+  }
+
+  return;
 }
 
-export async function createProject(options) {
- options = {
-   ...options,
-   targetDirectory: options.targetDirectory || process.cwd()
- };
 
- const templateDir = path.resolve(
-   new URL(import.meta.url).pathname,
-   '../../../templates',
-   options.template
- );
- options.templateDirectory = templateDir;
+export async function process(options) {
 
- try {
-   await access(templateDir, fs.constants.R_OK);
- } catch (err) {
-   console.error('%s Invalid template name', chalk.red.bold('ERROR'));
-   process.exit(1);
- }
+  const tasks = new Listr([
+    {
+      title: 'Initialize completium',
+      task: () => initCompletium(options),
+      enabled: () => options.init,
+    },
+    {
+      title: 'Deployment from archetype file',
+      task: () => deploy(options),
+      enabled: () => options.deploy,
+    }
+  ]);
 
- const tasks = new Listr([
-   {
-     title: 'Copy project files',
-     task: () => copyTemplateFiles(options),
-   },
-   {
-     title: 'Initialize git',
-     task: () => initGit(options),
-     enabled: () => options.git,
-   },
-   {
-     title: 'Install dependencies',
-     task: () =>
-       projectInstall({
-         cwd: options.targetDirectory,
-       }),
-     skip: () =>
-       !options.runInstall
-         ? 'Pass --install to automatically install dependencies'
-         : undefined,
-   },
- ]);
-
- await tasks.run();
- console.log('%s Project ready', chalk.green.bold('DONE'));
- return true;
+  await tasks.run();
+  // console.log('%s Project ready', chalk.green.bold('DONE'));
+  return true;
 }
