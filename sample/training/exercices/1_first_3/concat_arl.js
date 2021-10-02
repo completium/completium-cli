98ecc5f1aad5572da45ebe254666662771479486
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-10-02 18:37:00 +0200

    up

---
+++ sample/training/exercices/1_first_3/concat_arl.js
@@ -0,0 +1,12 @@
+const { deploy } = require('@completium/completium-cli');
+const assert = require('assert');
+
+const test = async () => {
+  const [contract, _] = await deploy('./sample/training/exercices/first_3/concat.arl');
+  await contract.concat({ arg: { p: "Julie" } });
+  await contract.concat({ arg: { p: "Jacques" } });
+  const storage = await contract.getStorage("concat");
+  assert(storage.text === "Hi, Julie, Jacques", "Invalid");
+  assert(storage.count.toNumber() === 2, "Invalid");
+}
+test();
