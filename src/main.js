d4bcd322e9ba35273eec6e4bec17102819c1ba6b
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2021-12-05 01:35:29 +0100

    inc version

--- src/main.js
+++ src/main.js
@@ -18,7 +18,7 @@ const signer = require('@taquito/signer');
 const { BigNumber } = require('bignumber.js');
 let archetype = null;
 
-const version = '0.3.7'
+const version = '0.3.8'
 
 const homedir = require('os').homedir();
 const completium_dir = homedir + '/.completium'
