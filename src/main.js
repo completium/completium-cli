24d08e6dd9b40e4ca0ab92ad5f9bc6f36734996f
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2023-02-06 10:53:49 +0100

    inc version

--- src/main.js
+++ src/main.js
@@ -20,7 +20,7 @@ const { BigNumber } = require('bignumber.js');
 const { Fraction } = require('fractional');
 let archetype = null;
 
-const version = '0.4.73'
+const version = '0.4.74'
 
 const homedir = require('os').homedir();
 const completium_dir = homedir + '/.completium'
