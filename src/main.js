963d8588b2a1f1ea5e0f7c4f61c5fef3abbc53e0
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2024-04-10 15:41:00 +0200

    up

--- src/main.js
+++ src/main.js
@@ -43,8 +43,8 @@ const context_mockup_path = completium_dir + "/mockup/mockup/context.json";
 
 const tezos_client_dir = homedir + '/.tezos-client'
 
-// const default_mockup_protocol = 'ProxfordYmVfjWnRcgjWH36fW6PArwqykTFzotUxRs6gmTcZDuH'
-const default_mockup_protocol = 'PtNairobiyssHuh87hEhfVBGCVrK3WnS8Z2FT4ymB5tAa4r1nQf'
+// const default_mockup_protocol = 'PtParisBQscdCm6Cfow6ndeU6wKJyA3aV1j4D3gQBQMsTQyJCrz'
+const default_mockup_protocol = 'ProxfordYmVfjWnRcgjWH36fW6PArwqykTFzotUxRs6gmTcZDuH'
 
 const import_endpoint = 'https://ghostnet.ecadinfra.com'; // used for import faucet
 
